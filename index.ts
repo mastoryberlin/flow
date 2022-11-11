698bac5f9a659eeeaacb6618ea65e4424c8af1fc
Makar <makar.vakulenko@erbis.com>
2022-11-11 11:32:57 +0200

    Added flowToLocale js version

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { useParser } from "./src/chevrotain/Parser"
 import { useVisitor } from './src/chevrotain/Visitor'
 
 import { useFlowToStatechart } from "./src/processing/statechart";
-import { useFlowToLocale } from "./src/processing/locale";
+import  useFlowToLocale  from "./src/processing/locales";
 
 export { useTokens, useLexer, useParser, useVisitor, useFlowToStatechart, useFlowToLocale }
 export * from "./src/dsl/types"

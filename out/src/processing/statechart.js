453e1558270dc58a5db72a1c6163e8d22b347db2
Makar <makar.vakulenko@erbis.com>
2023-08-01 14:23:16 +0300

    New release version

--- out/src/processing/statechart.js
+++ out/src/processing/statechart.js
@@ -327,8 +327,8 @@ function stateNodeToJsonRecursive(fqPath, variant, node, parentInfo) {
                 actions: {
                     unquoted: true,
                     raw: "assign({ \n              __interpolatedMessage: ".concat(kind === 'text' ?
-                        (0, unit_context_1.evaluateInContext)('`' + node.message.text.replace(/`/g, '\\`') + '`') :
-                        ((_b = node.message.source) === null || _b === void 0 ? void 0 : _b.toString()) || '', "})")
+                        "".concat((0, unit_context_1.evaluateInContext)('`' + node.message.text.replace(/`/g, '\\`') + '`')) :
+                        "".concat((_b = node.message.source) === null || _b === void 0 ? void 0 : _b.toString()) || '', "\n          })")
                 }
             };
         }

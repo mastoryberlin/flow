1f30e8ef0992a1ef155627ececa6e9525f46b3ca
Felix Schwarz <felix@mastory.io>
2025-04-21 23:14:16 +0200

    chore: remove vue deps

--- src/components/__tests__/parser.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-// describe('HelloWorld', () => {
-//   it('renders properly', () => {
-//     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-//     expect(wrapper.text()).toContain('Hello Vitest')
-//   })
-// })
